cfbe960474b35d65ebbf1d0ced23e46b90ee7700
Hellol77 <dhe7700@naver.com>
2022-02-03 21:33:36 +0900

    add delete

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.post("/add", function (요청, 응답) {
       console.log(결과.totalPost);
       var 총게시물갯수 = 결과.totalPost;
       db.collection("post").insertOne(
-        { _id: 총게시물갯수+1, 할일: 요청.body.title, 날짜: 요청.body.date },
+        { _id: 총게시물갯수 + 1, 할일: 요청.body.title, 날짜: 요청.body.date },
 
         function (에러, 결과) {
           console.log("저장완료");
@@ -60,4 +60,12 @@ app.get("/list", function (요청, 응답) {
       console.log(결과);
       응답.render("list.ejs", { posts: 결과 });
     });
+});
+
+app.delete("/delete", function (요청, 응답) {
+  요청.body._id = parseInt(요청.body._id);
+  db.collection("post").deleteOne(요청.body, function (에러, 결과) {
+    console.log("삭제완료");
+    응답.status(200).send({message : '성공했습니다.'});  
+  });
 });
