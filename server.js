2d2e324363c5187a5fe74f2a37d51beb0a1e556f
Hellol77 <dhe7700@naver.com>
2022-02-04 02:02:32 +0900

    add style

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const MongoClient = require("mongodb").MongoClient;
 var db;
 app.set("view engine", "ejs");
 
+app.use("/public", express.static("public"));
+
 MongoClient.connect(
   "mongodb+srv://dhe77:wona669700@cluster0.pap4p.mongodb.net/cluster0?retryWrites=true&w=majority",
   function (에러, client) {
@@ -15,13 +17,10 @@ MongoClient.connect(
       console.log("listening on 8080");
     });
     app.get("/", function (요청, 응답) {
-      응답.sendFile(__dirname + "/index.html");
+      응답.sendFile(__dirname + "views/index.ejs");
     });
     app.get("/write", function (요청, 응답) {
-      응답.sendFile(__dirname + "/write.html");
-    });
-    app.get("/pet", function (요청, 응답) {
-      응답.send("펫용품");
+      응답.sendFile(__dirname + "views/write.ejs");
     });
   }
 );
@@ -66,6 +65,16 @@ app.delete("/delete", function (요청, 응답) {
   요청.body._id = parseInt(요청.body._id);
   db.collection("post").deleteOne(요청.body, function (에러, 결과) {
     console.log("삭제완료");
-    응답.status(200).send({message : '성공했습니다.'});  
+    응답.status(200).send({ message: "성공했습니다." });
   });
+});
+
+app.get("/detail/:id", function (요청, 응답) {
+  db.collection("post").findOne(
+    { _id: parseInt(요청.params.id) },
+    function (에러, 결과) {
+      console.log(결과);
+      응답.render("detail.ejs", { data: 결과 });
+    }
+  );
 });
