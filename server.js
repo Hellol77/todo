3024ecb3bbc2222f169c30168eeedbd9b78a875e
Hellol77 <dhe7700@naver.com>
2022-02-04 20:51:16 +0900

    수정하기

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ app.use(express.urlencoded({ extended: true }));
 const MongoClient = require("mongodb").MongoClient;
 var db;
 app.set("view engine", "ejs");
-
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
 app.use("/public", express.static("public"));
 
 MongoClient.connect(
@@ -17,10 +18,12 @@ MongoClient.connect(
       console.log("listening on 8080");
     });
     app.get("/", function (요청, 응답) {
-      응답.sendFile(__dirname + "views/index.ejs");
+      // 응답.sendFile(__dirname + "/views/index.ejs");
+      응답.render("index.ejs");
     });
     app.get("/write", function (요청, 응답) {
-      응답.sendFile(__dirname + "views/write.ejs");
+      // 응답.sendFile(__dirname + "/views/write.ejs");
+      응답.render("write.ejs");
     });
   }
 );
@@ -78,3 +81,37 @@ app.get("/detail/:id", function (요청, 응답) {
     }
   );
 });
+
+app.get("/edit/:id", function (요청, 응답) {
+  db.collection("post").findOne(
+    { _id: parseInt(요청.params.id) },
+    function (에러, 결과) {
+      응답.render("edit.ejs", { post: 결과 });
+    }
+  );
+});
+
+app.put("/edit", function (요청, 응답) {
+  db.collection("post").updateOne(
+    { _id: parseInt(요청.body.id) },
+    { $set: { 할일: 요청.body.title, 날짜: 요청.body.date } },
+    function (에러, 결과) {
+      console.log("수정완료");
+      응답.redirect("/list");
+    }
+  );
+});
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+
+app.use(
+  session({ secret: "비밀코드", resave: true, saveUninitialized: false })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.post('/login',function(요청, 응답){
+
+})
